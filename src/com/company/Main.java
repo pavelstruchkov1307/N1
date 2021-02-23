005bc5ca0e2b03e421db9d31e99f3adbc1b6b71c
pavelstruchkov1307 <pavel.struchkov1999@gmail.com>
2021-02-23 22:07:59 +0300

    fix bag

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        String nomer = "+7 (924) 6827 823";
+        String nomer = "+7 (924) 6827 8234";
         System.out.println(Arrays.toString(clear(nomer)));
     }
 
     public static String[] clear(String nom) {
         String[] arrStr = new String[2];
         String mess = "";
+        String sizeNom="";
 
         if (nom.startsWith("+7")) {
             nom = nom.replace("+7", "8");
@@ -21,11 +22,11 @@ public class Main {
         if (nom.contains(" ") || nom.contains(")") || nom.contains("(")) {
             nom = nom.replaceAll("\\s+", "");
             nom = nom.replaceAll("\\p{P}", "");
-            mess += " В номере есть пробелы и/или скобки";
+            mess += " В номере есть пробелы и/или скобки;";
         }
 
         if (nom.length() > 11) {
-            mess = "Количество символов больше 11";
+            sizeNom = " Количество символов больше 11:кол-во символов="+ nom.length();
         }
 
         if(mess.equals("")){
@@ -35,10 +36,10 @@ public class Main {
         if (nom.length() == 11) {
             arrStr[0] = nom;
         } else {
-            arrStr[0] = "Введен некорректный номер";
+            arrStr[0] = "Введен некорректный номер:кол-во символов="+ nom.length();
         }
 
-        arrStr[1] = mess;
+        arrStr[1] = mess+sizeNom;
 
         return arrStr;
 
